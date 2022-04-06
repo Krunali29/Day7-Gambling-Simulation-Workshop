118fb21e121381eb649a74ac6a092ea653fee9c3
Krunali <krunalilole29@gmail.com>
2022-04-06 20:45:14 +0530

    Adding UC 2 To Win or Loose

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -4,7 +4,19 @@ public class GamblingSimulator
 {
     public static final  int INITIAL_STAKE = 100;
     public static final int STAKE_BET = 1;
+    public static int stake = 0;
 
+    public static void winOrLose()
+    {
+        if(Math.random()<0.5) {
+            stake++;
+            System.out.println("Win");;
+        }
+        else {
+            stake --;
+            System.out.println("Lose");
+        }
+    }
     public static void main(String[] args)
     {
         System.out.println("Welcome To Gambler Simulation");
@@ -12,3 +24,4 @@ public class GamblingSimulator
         System.out.println("Stake Bet is " +STAKE_BET+"$");
     }
 }
+
