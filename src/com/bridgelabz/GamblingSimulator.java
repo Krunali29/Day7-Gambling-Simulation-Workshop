a6408ccb3afb1a1bf96b0c9b05f7bec0e8337ae6
Krunali <krunalilole29@gmail.com>
2022-04-07 17:53:19 +0530

    Adding UC 3 To Win or Loose 50%

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -2,26 +2,33 @@ package com.bridgelabz;
 
 public class GamblingSimulator
 {
-    public static final  int INITIAL_STAKE = 100;
-    public static final int STAKE_BET = 1;
-    public static int stake = 0;
+    public static final int STAKE=100;
+    public static final int BET=1;
+    public static final int PERCENTAGE_50_STAKE=(STAKE/100)*50;
+    public static final int HIGH_LIMIT=STAKE+PERCENTAGE_50_STAKE;
+    public static final int LOW_LIMIT=STAKE-PERCENTAGE_50_STAKE;
 
-    public static void winOrLose()
-    {
-        if(Math.random()<0.5) {
-            stake++;
-            System.out.println("Win");;
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Gambler Game");
+        int totalStake = STAKE;
+
+        while (totalStake < HIGH_LIMIT && totalStake > LOW_LIMIT)
+        {
+            int game = (int) (Math.random() * 10) % 2;
+            if (game == 1) {
+                totalStake = totalStake + BET;
+                System.out.println("Gambler won the bet and having total stack " + totalStake);
+            } else {
+                totalStake = totalStake - BET;
+                System.out.println("Gambler lost the bet and having total stack " + totalStake);
+            }
         }
-        else {
-            stake --;
-            System.out.println("Lose");
+        if(totalStake==HIGH_LIMIT){
+            System.out.println("Gambler won by "+totalStake);
+        } else
+        {
+            System.out.println("Gambler lost by "+totalStake);
         }
-    }
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome To Gambler Simulation");
-        System.out.println("Initial Stake is " +INITIAL_STAKE +"$");
-        System.out.println("Stake Bet is " +STAKE_BET+"$");
     }
 }
 
